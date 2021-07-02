beeca401eb91a7b4c15434598d1610c34c8f13b7
Arul7979 <khairulridha79@gmail.com>
2021-07-02 17:23:27 +0800

    Update views.py

--- external/views.py
+++ external/views.py
@@ -1,9 +1,10 @@
-from django.http import request
 from django.shortcuts import render
 
 # Create your views here.
+
+def error404(request):
+    return render(request, "external/airui/system-404.html")
 def login(request):
     return render(request, "external/airui/login.html")
-
 def tables(request):
     return render(request, "external/airui/tables.html")    
