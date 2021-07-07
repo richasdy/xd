593c267498a1c6f41dec0c36d89d127702212a51
howardjeremy10 <howardjeremy10@gmail.com>
2021-07-07 07:53:56 +0700

    upload internal

---
+++ internal/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from . import views
+
+app_name = 'internal'
+urlpatterns = [
+
+    # # ex: /polls/
+    # path('', views.index, name='index'),
+    # # ex: /polls/5/
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # # ex: /polls/5/results/
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # # ex: /polls/5/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # path('', views.IndexView.as_view(), name='index'),
+    # path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    # path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    
+    path('DirectorateofPostgraduate&AdvanceLearning', views.DirectorateofPostgraduate&AdvanceLearning, name='DirectorateofPostgraduate&AdvanceLearning'),
+    path('DirectorateofFinance', views.DirectorateofFinance, name='DirectorateofFinance'),
+    path('DirectorateofCareer,Alumni&EndowmentDevelopment', views.DirectorateofCareer,Alumni&EndowmentDevelopment, name='DirectorateofCareer,Alumni&EndowmentDevelopment'),
+    path('DirectorateofResearchandCommunityService', views.DirectorateofResearchandCommunityService, name='DirectorateofResearchandCommunityService'),
+    path('ResearchGroup', views.ResearchGroup, name='ResearchGroup'),
+]
---
+++ internal/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def DirectorateofPostgraduate&AdvanceLearning(request):
+    return render(request, "internal/DirectorateofPostgraduateandAdvanceLearning.html")
+def DirectorateofFinance(request):
+    return render(request, "internal/DirectorateofFinance.html")
+def DirectorateofCareer,Alumni&EndowmentDevelopment(request):
+    return render(request, "internal/DirectorateofCareer,Alumni&EndowmentDevelopment.html")
+def DirectorateofResearchandCommunityService(request):
+    return render(request, "internal/DirectorateofResearchandCommunityService.html")
+def ResearchGroup(request):
+    return render(request, "internal/ResearchGroup.html")
