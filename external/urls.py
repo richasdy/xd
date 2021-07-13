15d56e09a6bcdbc231eec4cc1c367a41da60bbae
Khairul Ridha M <75602944+khairulridha@users.noreply.github.com>
2021-07-13 12:15:10 +0800

    Khairul (#12)
    * nambah card histogram
    
    * menambah app internal dan dashboard
    
    * mengubah data source d3 histogram menjadi json
    
    * resolve internal app conflicts & fix external app
    
    Co-authored-by: Eko Hardiyanto <ehardi19@gmail.com>

--- external/urls.py
+++ external/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('tables', views.tables, name='tables'),
     path('form', views.form, name='form'),
     path('dashboard', views.dashboard, name='dashboard'),
+    path('youtube', views.dashboard_youtube, name='youtube'),
     path('tiktok', views.dashboard_tiktok, name='tiktok'),
-    path('portal_berita', views.portal_berita, name='portal_berita'),
+    path('portal-berita', views.dashboard_portal_berita, name='portal_berita'),
 ]
--- external/views.py
+++ external/views.py
@@ -2,15 +2,24 @@ from django.shortcuts import render
 
 def error404(request):
     return render(request, "external/airui/system-404.html")
+
 def login(request):
     return render(request, "external/airui/system-login.html")
+
 def tables(request):
     return render(request, "external/airui/tables-datatables.html")
+
 def form(request):
     return render(request, "external/airui/form-plugins-select2.html")
+
 def dashboard(request):
     return render(request, "external/airui/dashboards-analytics.html")
+
 def dashboard_tiktok(request):
-    return render(request, "external/airui/dashboards-tiktok.html")
-def portal_berita(request):
-    return render(request, "external/airui/portal_berita.html")
+    return render(request, "external/dashboard-tiktok.html")
+
+def dashboard_youtube(request):
+    return render(request, "external/dashboard-youtube.html") 
+
+def dashboard_portal_berita(request):
+    return render(request, "external/dashboard-portal-berita.html")
