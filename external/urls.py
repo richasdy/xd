52d296808ad664340b57444ca36993fb4482c603
Jose Purba <josprb@gmail.com>
2021-07-14 05:48:27 +0700

    add login and register features

--- external/urls.py
+++ external/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 from . import views
 
+from django.contrib.auth import views as auth_views
+
 app_name = 'external'
 urlpatterns = [
     path('dashboard', views.dashboard, name='dashboard'),
     path('error404', views.error404, name='error404'),
     path('form', views.form, name='form'),
-    path('login', views.login, name='login'),
+    path('auth/login/', auth_views.LoginView.as_view(template_name='external/airui/system-login.html'), name='login'),
+    path('auth/logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('auth/register', views.sign_up,name="register"),
     path('tables', views.tables, name='tables'),
     path('instagram', views.instagram, name='instagram'),
     path('portal-berita', views.portal_berita, name='portal_berita'),
--- external/views.py
+++ external/views.py
@@ -1,4 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.sites.models import Site
+from django.db import models
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate
+from django.contrib import messages
+from django.urls import reverse
+
+def sign_up(request):
+    current_site = Site.objects.get_current(request)
+    context = {}
+    context['getURL'] = current_site
+    form = UserCreationForm(request.POST or None)
+    context['registered'] = False
+    if request.method == "POST":
+        if form.is_valid():
+            user = form.save()
+            context['registered'] = True
+    context['form'] = form
+    return render(request,'external/airui/system-register.html', context)
 
 def dashboard(request):
     return render(request, "external/airui/dashboards-analytics.html")
@@ -8,9 +28,6 @@ def error404(request):
 
 def form(request):
     return render(request, "external/airui/form-plugins-select2.html")
-
-def login(request):
-    return render(request, "external/airui/system-login.html")
 
 def tables(request):
     return render(request, "external/airui/tables-datatables.html")
@@ -25,7 +42,10 @@ def tiktok(request):
     return render(request, "external/tiktok.html")
 
 def twitter(request):
-	return render(request, "external/twitter.html")
+    current_site = Site.objects.get_current(request)
+    context = {}
+    context['getURL'] = current_site
+    return render(request, "external/twitter.html", context)
 
 def youtube(request):
     return render(request, "external/youtube.html") 
