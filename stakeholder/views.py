aa074d00c6c132390644bb98cf18b4642ac10572
Eko Hardiyanto <ehardi19@gmail.com>
2021-07-27 02:13:41 +0700

    implement dashboard mahasiswa terhadap visi dan misi

--- stakeholder/views.py
+++ stakeholder/views.py
@@ -12,3 +12,7 @@ logger = logging.getLogger(__name__)
 @login_required
 def mitra(request):
     return render(request, "stakeholder/mitra.html")
+
+@login_required
+def mahasiswa_visi_misi(request):
+    return render(request, "stakeholder/mahasiswa-visi-misi.html")
