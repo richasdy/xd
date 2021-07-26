2e3f93bdc37364d026585dc7bde44b85e609064d
fathurrohman4 <mfathurrhmnnn@student.telkomuniversity.ac.id>
2021-07-26 19:57:40 +0700

    +Update
    Pegawai + MhsLayanan

--- stakeholder/urls.py
+++ stakeholder/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'stakeholder'
 urlpatterns = [
     path('pegawai', views.pegawai, name='pegawai'),
+    path('mahasiswalayanan', views.mahasiswalayanan, name='mahasiswalayanan'),
 ]
--- stakeholder/views.py
+++ stakeholder/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+import json
 
 def pegawai(request):
     return render(request, "stakeholder/stakeholder-pegawai.html")
+
+def mahasiswalayanan(request):
+    return render(request, "stakeholder/stakeholder-mahasiswalayanan.html")
