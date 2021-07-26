8bb2bdabefc493d6820a0604e005192e7a5edbdd
fathurrohman4 <mfathurrhmnnn@student.telkomuniversity.ac.id>
2021-07-26 09:03:04 +0700

    Change Color
    Change Color

--- stakeholder/views.py
+++ stakeholder/views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-import logging
-import json
-import os
-from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
 
 def pegawai(request):
     return render(request, "stakeholder/stakeholder-pegawai.html")
